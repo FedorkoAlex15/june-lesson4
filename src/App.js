e3658fe4e882ee2cd91b94b2e3b615959f29fafb
AlexFedorko <alexfedorko15@gmail.com>
2021-08-21 22:58:47 +0300

    git init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
-import {createRef, useState} from "react";
-import {saveUser} from "./services/user.api.server";
-import UncontrolledForm from "./uncontrolledForm/uncontrolledForm";
-import SuperForm from "./superForm/SuperForm";
+
+import {createCar, getCar} from "./services/services/cars.service";
+import {useEffect, useState} from "react";
 
 function App() {
 
+    const [cars, setCars] = useState([])
+
+    let [model, setModel] = useState('')
+    let [price, setPrice] = useState(0)
+    let [year, setYear] = useState(0)
+
+
+    const carInfo = {
+        model: model,
+        price: price,
+        year: year
+    }
+
+
+    const save = (e) => {
+        e.preventDefault()
+        createCar(carInfo)
+
+    }
+
+
+    useEffect(() => {
+        getCar().then(value => setCars([...value]))
+
+    }, [])
+
+
+    // useEffect(() => {
+    //     createCar(carInfo).then(value => setCars(value))
+    // }, [])
+    // //
+
+
+
+    let onModelChange = (e) => {
+        setModel(e.target.value)
+    }
+    let onPriceChange = (e) => {
+        setPrice(e.target.value)
+    }
+    let onYearChange = (e) => {
+        setYear(e.target.value)
+    }
+
+
+
+
+    console.log(cars)
 
 
     return (
+        <div className="App">
+            <form onSubmit={save}>
+                <input type="text" name={'model'} value={model} onChange={onModelChange}/>
+                <input type="number" name={'price'} value={price} onChange={onPriceChange}/>
+                <input type="number" name={'year'} value={year} onChange={onYearChange}/>
+                <input type="submit"/>
+            </form>
 
-    <div className="App">
-        <SuperForm/>
-    </div>
-  );
+
+            {
+                cars.map(value => {
+                    return <div>{value.model}</div>
+                })
+            }
+
+        </div>
+    );
 }
+
 
 export default App;
---
+++ src/services/services/cars.service.js
@@ -0,0 +1,28 @@
+const getCar = () => {
+ return  fetch('http://91.201.233.14//api/v1/cars')
+        .then(value => value.json())
+}
+
+
+const createCar = (carInfo) => {
+ fetch('http://91.201.233.14//api/v1/cars', {
+  method: 'POST',
+  body: JSON.stringify(carInfo),
+
+  headers: {
+   'Content-type': 'application/json; charset=UTF=8',
+  },
+ })
+
+     .then(response => response.json())
+      .then(json => console.log(json))
+
+
+}
+
+
+
+export {getCar, createCar}
+
+
+
